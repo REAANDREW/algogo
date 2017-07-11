eb0ce991fd64b4603c091d46bde6e7ae58b3391e
Andrew Rea <email@andrewrea.co.uk>
2017-07-11 11:46:05 +0000

    implementation of selection sort

---
+++ SelectionSort_test.go
@@ -0,0 +1,36 @@
+package algogo_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func SelectionSort(input []int) (returnValue []int) {
+
+	returnValue = input
+
+	for i := 0; i < len(input); i++ {
+		var minJ = i
+		for j := i + 1; j < len(input); j++ {
+			if returnValue[j] < returnValue[minJ] {
+				minJ = j
+			}
+		}
+
+		var minJValue = returnValue[minJ]
+		returnValue[minJ] = returnValue[i]
+		returnValue[i] = minJValue
+	}
+
+	returnValue = input
+	return
+}
+
+func TestSelectionSort(t *testing.T) {
+	var input = []int{5, 4, 3, 2, 1}
+	var expected = []int{1, 2, 3, 4, 5}
+	var output = SelectionSort(input)
+
+	assert.Equal(t, expected, output)
+}
